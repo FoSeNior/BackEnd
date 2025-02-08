2b6943addb21f5454e5bf7cfc296ae4986d7f9ae
hui-jeong <huijeong1028@gmail.com>
2025-02-09 04:04:34 +0900

    feat: UserRepository에 userId 중복 확인 및 userId로 사용자 조회 메소드 추가

--- src/main/java/com/inje/forseni/Repository/UserRepository.java
+++ src/main/java/com/inje/forseni/Repository/UserRepository.java
@@ -4,6 +4,13 @@ import com.inje.forseni.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    //  userId 중복 확인
+    boolean existsByMembershipId(String membershipId);
+
+    //  userId로 사용자 찾기
+    Optional<User> findByMembershipId(String membershipId);
 }
