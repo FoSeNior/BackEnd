96befbe50bb356bd79fb62936d7472337aa6c228
unji3841 <unji3841@naver.com>
2025-02-09 17:01:52 +0900

    init

---
+++ src/main/java/com/inje/forseni/ForseniApplication.java
@@ -0,0 +1,13 @@
+package com.inje.forseni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForseniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForseniApplication.class, args);
+	}
+
+}
