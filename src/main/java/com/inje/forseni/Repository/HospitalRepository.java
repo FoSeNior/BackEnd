1bad6afcfee85dfb0a9ca47b52b6a63021715692
hui-jeong <huijeong1028@gmail.com>
2025-02-18 20:12:38 +0900

    add: HospitalRepository에 커스텀 쿼리 메서드 추가

--- src/main/java/com/inje/forseni/Repository/HospitalRepository.java
+++ src/main/java/com/inje/forseni/Repository/HospitalRepository.java
@@ -2,8 +2,19 @@ package com.inje.forseni.Repository;
 
 import com.inje.forseni.Entity.Hospital;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
+    //boolean existsByhNameAndhAddressAndhNumber(String hName, String hAddress, String hNumber);
+
+    @Query("SELECT h FROM Hospital h WHERE h.hName = :hName AND h.hAddress = :hAddress AND h.hNumber = :hNumber")
+    Optional<Hospital> findByHospitalDetails(@Param("hName") String hName, @Param("hAddress") String hAddress, @Param("hNumber") String hNumber);
+
+    @Query("select  count(h) > 0 from Hospital h where h.hName = :hName and h.hAddress = :hAddress and h.hNumber = :hNumber")
+    boolean existsByHospital(@Param("hName") String hName, @Param("hAddress")String hAddress, @Param("hNumber") String hNumber);
 }
